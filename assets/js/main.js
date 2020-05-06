97fd5cfe98a8f3c47edb0f4abc8365ba7c83e34e
Ivan Lyutskanov <Van@Ivans-MacBook-Air.local>
2020-05-06 13:01:20 +0300

    regular update and clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,21 @@
-jQuery(document).ready(function($) {
-
-
-    /*======= Skillset *=======*/
-
-
-    $('.level-bar-inner').css('width', '0');
-
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-
-            var itemWidth = $(this).data('level');
-
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-
-        });
-
-    });
-
-
-
+window.addEventListener("DOMContentLoaded", function () {
+  var delay = 300;
+  document.querySelectorAll(".level-bar-inner").forEach(function (bar) {
+    animateFill(bar, bar.dataset.level, 7, delay);
+    delay += 100;
+  });
 });
+
+function animateFill(element, level, speed, delay) {
+  setTimeout(function () {
+    var progress = 0;
+    function frame() {
+      progress++;
+      element.style.width = progress + "%";
+      if (progress == level) {
+        clearInterval(fill);
+      }
+    }
+    var fill = setInterval(frame, speed);
+  }, delay);
+}
